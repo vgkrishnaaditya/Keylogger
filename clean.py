758cb38034f1e37022cf734a6ed21b484710e62b
vgkrishnaaditya <149256923+vgkrishnaaditya@users.noreply.github.com>
2024-03-12 20:21:08 +0530

    Add files via upload

---
+++ clean.py
@@ -0,0 +1,11 @@
+class clean:
+
+    def cleaning(self):
+        with open("log.txt", 'w') as fl:
+            print("Cleared");
+
+    def main(self):
+    	obj.cleaning();
+if(__name__=="__main__"):
+	obj = clean()
+	obj.main()
